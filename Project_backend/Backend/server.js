355b90419010274470eea90802b82af02db52dcc
Nilanshi Jain <nilanshijain31122@gmail.com>
2024-07-25 13:03:34 +0530

    add these backend files

---
+++ Project_backend/Backend/server.js
@@ -0,0 +1,50 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const app = express();
+
+app.use(bodyParser.json());
+app.use(cors());
+
+mongoose
+  .connect("mongodb://localhost:27017/mydb", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("MongoDB connected"))
+  .catch((err) => console.log(err));
+
+const UserSchema = new mongoose.Schema({
+  firstname: String,
+  lastname: String,
+  username: String,
+  email: String,
+  password: String,
+});
+
+const User = mongoose.model("User", UserSchema);
+
+app.post("/api/signup", (req, res) => {
+  const { firstname, lastname, username, email, password } = req.body;
+
+  const newUser = new User({
+    firstname,
+    lastname,
+    username,
+    email,
+    password,
+  });
+
+  newUser
+    .save()
+    .then((user) => res.json(user))
+    .catch((err) => res.status(400).json({ error: err.message }));
+});
+
+app.get("/", (req, res) => {
+  res.json("Nilanshi is a developer");
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
